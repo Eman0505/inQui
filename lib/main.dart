80a01d9e86febab5aa1ad4b60d375e6961d21b5b
Eman <104957698+Eman0505@users.noreply.github.com>
2022-05-31 17:10:37 +0800

    BGM codes, naka // yung codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:investor_quizapp/pages/history.dart';
+import 'package:flutter/services.dart';
+import 'package:investor_quizapp/pages/historyCategory.dart';
 import 'package:investor_quizapp/pages/home.dart';
 import 'package:investor_quizapp/pages/profile.dart';
 import 'package:investor_quizapp/pages/settings.dart';
-import 'package:investor_quizapp/data/detail.dart'; 
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+//import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  Hive.registerAdapter(DetailAdapter());
-  await Hive.openBox<Detail>('details');
+  await Hive.openBox('Personal_Finance');
+  await Hive.openBox('Investment_and_Portfolio_Management');
+  await Hive.openBox('Behavioral_Finance');
+  await Hive.openBox('Capital_Market');
   runApp(const MyApp());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Image.asset('assets/images/logo_animated.gif',
+          height: 280, width: 280),
+      backgroundColor: Colors.white,
+      nextScreen: const MainPage(),
+      splashIconSize: 280,
+      duration: 3000, //1000 = 1 sec (default 2500)
+      pageTransitionType: PageTransitionType.leftToRightWithFade,
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -20,13 +41,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Investor Quiz App',
       theme: ThemeData(
         primarySwatch: Colors.green,
         scaffoldBackgroundColor: const Color.fromRGBO(254, 254, 254, 1),
       ),
-      home: const MainPage(),
+      home: const SplashScreen(),
+      //Kung debugging pwede paltan home ng MainPage()
     );
   }
 }
@@ -38,57 +64,97 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin {
+  //final AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();  //// this will create a instance object of a class
+  //@override
+  //void initState() {
+    //// TODO: implement initState
+    //super.initState();
+    //audioPlayer.open(Audio('assets/music/Enjoy_the_Experience.mp3'));
+  //}
+
   int selectedIndex = 0;
   final screens = [
     HomePage(),
-    const HistoryPage(),
+    const HistoryPages(),
     const ProfilePage(),
     const SettingsPage(),
   ];
+  DateTime? lastPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: selectedIndex,
-        children: screens,
-      ),
-      bottomNavigationBar: NavigationBarTheme(
-        data: NavigationBarThemeData(
-          indicatorColor: const Color.fromRGBO(81, 231, 168, 1),
-          labelTextStyle: MaterialStateProperty.all(const TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 14,
-              fontWeight: FontWeight.bold)),
+    return WillPopScope(
+      onWillPop: () async {
+        final now = DateTime.now();
+        const maxDuration = Duration(seconds: 2);
+        final isWarning =
+            lastPressed == null || now.difference(lastPressed!) > maxDuration;
+
+        if (isWarning) {
+          lastPressed = DateTime.now();
+          const snackBar = const SnackBar(
+            content: Text('Double Tap to Close App'),
+            duration: maxDuration,
+          );
+
+          ScaffoldMessenger.of(context)
+            ..removeCurrentSnackBar()
+            ..showSnackBar(snackBar);
+
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        body: IndexedStack(
+          index: selectedIndex,
+          children: screens,
         ),
-        child: NavigationBar(
-          height: 70,
-          selectedIndex: selectedIndex,
-          onDestinationSelected: (index) =>
-              setState(() => selectedIndex = index),
-          destinations: const [
-            NavigationDestination(
-                icon: Icon(Icons.home_outlined),
-                selectedIcon: Icon(Icons.home_rounded),
-                label: 'Home'),
-            NavigationDestination(
-                icon: Icon(Icons.content_paste_rounded),
-                selectedIcon: Icon(Icons.content_paste_search_rounded),
-                label: 'History'),
-            NavigationDestination(
-                icon: Icon(Icons.person_outline_rounded),
-                selectedIcon: Icon(Icons.person_rounded),
-                label: 'Profile'),
-            NavigationDestination(
-                icon: Icon(Icons.settings_outlined),
-                selectedIcon: Icon(Icons.settings_rounded),
-                label: 'Settings'),
-          ],
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+            indicatorColor: const Color.fromRGBO(81, 231, 168, 1),
+            labelTextStyle: MaterialStateProperty.all(const TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 14,
+                fontWeight: FontWeight.bold)),
+          ),
+          child: NavigationBar(
+            height: 70,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (index) =>
+                setState(() => selectedIndex = index),
+            destinations: const [
+              NavigationDestination(
+                  icon: Icon(Icons.home_outlined),
+                  selectedIcon: Icon(Icons.home_rounded),
+                  label: 'Home'),
+              NavigationDestination(
+                  icon: Icon(Icons.content_paste_rounded),
+                  selectedIcon: Icon(Icons.content_paste_search_rounded),
+                  label: 'History'),
+              NavigationDestination(
+                  icon: Icon(Icons.person_outline_rounded),
+                  selectedIcon: Icon(Icons.person_rounded),
+                  label: 'Profile'),
+              NavigationDestination(
+                  icon: Icon(Icons.settings_outlined),
+                  selectedIcon: Icon(Icons.settings_rounded),
+                  label: 'Settings'),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  //@override
+  //void dispose() {
+    //// TODO: implement dispose
+    //audioPlayer.dispose();
+    //super.dispose();
+  //}
 }
 
 // App Color Scheme
